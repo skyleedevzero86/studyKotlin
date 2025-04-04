ef8a42cfc706b8a3b034bcf74dbe34004c4b4e53
이경용 <sleekydz86@naver.com>
2025-04-05 00:59:35 +0900

    feat:gradle정리

--- gorks/build.gradle.kts
+++ gorks/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    war
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "com.chaorks"
@@ -10,7 +12,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(19)
     }
 }
 
@@ -19,11 +21,42 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.boot:spring-boot-starter-webflux")
+        implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+        implementation("org.springframework.boot:spring-boot-starter-json")
+
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("com.fasterxml:classmate:1.5.1")
+
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+        implementation("org.mindrot:jbcrypt:0.4")
+
+        //implementation("mysql:mysql-connector-java:8.0.29")
+        //runtimeOnly("com.mysql:mysql-connector-j")
+        runtimeOnly("com.h2database:h2")
+
+        implementation("org.springframework.ai:spring-ai-openai-spring-boot-starter")
+
+        // Lombok (로그)
+        implementation("org.projectlombok:lombok:1.18.30")
+        annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+        // 개발 및 테스트
+        developmentOnly("org.springframework.boot:spring-boot-devtools")
+        providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("io.projectreactor:reactor-test")
+        testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+
 }
 
 kotlin {
