10eb0dbac5d6771b5657b8281480b08113b6db02
이경용 <sleekydz86@naver.com>
2025-04-09 22:48:00 +0900

    feat:Post버전 Init

---
+++ K_Post_Ai/src/main/kotlin/com/kposti/KPostAiApplication.kt
@@ -0,0 +1,11 @@
+package com.kposti
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KPostAiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KPostAiApplication>(*args)
+}
---
+++ K_Post_Ai/src/test/kotlin/com/kposti/KPostAiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kposti
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KPostAiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
