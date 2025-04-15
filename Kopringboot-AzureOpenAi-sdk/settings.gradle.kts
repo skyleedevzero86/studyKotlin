ddc4f7b50c5ac2226d8bf5ab6be4fd3fb1729550
이경용 <sleekydz86@naver.com>
2025-04-16 00:21:33 +0900

    feat:  Azure OpenAI 서비스프로젝트 셋팅

---
+++ Kopringboot-AzureOpenAi-sdk/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Kopringboot-AzureOpenAi-sdk"
---
+++ Kopringboot-AzureOpenAi-sdk/src/main/kotlin/com/azure/KopringbootAzureOpenAiSdkApplication.kt
@@ -0,0 +1,11 @@
+package com.azure
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringbootAzureOpenAiSdkApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringbootAzureOpenAiSdkApplication>(*args)
+}
