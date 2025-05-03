bf9420655b19dbdb330b332e88aecb71ba35fb25
이경용 <sleekydz86@naver.com>
2025-05-03 15:52:28 +0900

    feat: reg스터디 Init

---
+++ ragStudy/src/main/kotlin/com/ragstudy/RagStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.ragstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RagStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<RagStudyApplication>(*args)
+}
---
+++ ragStudy/src/test/kotlin/com/ragstudy/RagStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ragstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RagStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
