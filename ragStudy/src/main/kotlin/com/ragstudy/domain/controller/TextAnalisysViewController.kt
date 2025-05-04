448c847ab3b1990043ae42a0ce990ddd1f8e3779
이경용 <sleekydz86@naver.com>
2025-05-04 11:59:35 +0900

    fix:메인페이지 수정 및 결과페이지 생성

--- ragStudy/src/main/kotlin/com/ragstudy/domain/controller/TextAnalisysViewController.kt
+++ ragStudy/src/main/kotlin/com/ragstudy/domain/controller/TextAnalisysViewController.kt
@@ -1,6 +1,7 @@
 package com.ragstudy.domain.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
@@ -8,5 +9,10 @@ class TextAnalysisViewController {
     @GetMapping(value = ["/", "/talk"])
     fun getTextAnalysisPage(): String {
         return "rags/text-analysis"
+    }
+    
+    @GetMapping("/result")
+    fun getResultPage(model: Model): String {
+        return "rags/analysis-result"
     }
 }
