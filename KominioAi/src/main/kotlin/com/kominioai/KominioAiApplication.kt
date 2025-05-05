bb8a0cc6e9f9f28406e187767af603a97ce793a6
이경용 <sleekydz86@naver.com>
2025-05-06 00:08:36 +0900

    feat: Kotlin 기반 파일업로드 Init

---
+++ KominioAi/src/main/kotlin/com/kominioai/KominioAiApplication.kt
@@ -0,0 +1,11 @@
+package com.kominioai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KominioAiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KominioAiApplication>(*args)
+}
