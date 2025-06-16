5ab70b3296f39d60c7b78fab13e6625043a55900
이경용 <sleekydz86@icloud.com>
2025-06-16 23:29:50 +0900

    feat: voice Project Init

---
+++ ko_voice/src/main/kotlin/com/voice/KoVoiceApplication.kt
@@ -0,0 +1,11 @@
+package com.voice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KoVoiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<KoVoiceApplication>(*args)
+}
