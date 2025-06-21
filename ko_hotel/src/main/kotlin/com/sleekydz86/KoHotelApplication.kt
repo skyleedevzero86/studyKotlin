36e55061e5888ac37ed42f8b355173201ea2dd4a
이경용 <sleekydz86@icloud.com>
2025-06-21 14:36:00 +0900

    feat:reg study hodel version Init

---
+++ ko_hotel/src/main/kotlin/com/sleekydz86/KoHotelApplication.kt
@@ -0,0 +1,11 @@
+package com.sleekydz86
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KoHotelApplication
+
+fun main(args: Array<String>) {
+    runApplication<KoHotelApplication>(*args)
+}
