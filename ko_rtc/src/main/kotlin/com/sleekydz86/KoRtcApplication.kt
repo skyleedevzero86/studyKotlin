1b914ae1de12ce03e49565bcc804b1c5b4b481e2
이경용 <sleekydz86@icloud.com>
2025-06-27 02:23:00 +0900

    feat: 미디어서버 학습하기위해 셋팅

---
+++ ko_rtc/src/main/kotlin/com/sleekydz86/KoRtcApplication.kt
@@ -0,0 +1,11 @@
+package com.sleekydz86
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KoRtcApplication
+
+fun main(args: Array<String>) {
+    runApplication<KoRtcApplication>(*args)
+}
