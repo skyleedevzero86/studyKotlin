5b8f5ec8a2bbe2ad4a71af5e25e60387136b98f3
이경용 <sleekydz86@icloud.com>
2025-08-19 19:56:18 +0900

    feat: kotlin기반의 sse 2번째

---
+++ ko_sse2/src/main/kotlin/com/sleekydz86/KoSse2Application.kt
@@ -0,0 +1,11 @@
+package com.sleekydz86
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KoSse2Application
+
+fun main(args: Array<String>) {
+    runApplication<KoSse2Application>(*args)
+}
