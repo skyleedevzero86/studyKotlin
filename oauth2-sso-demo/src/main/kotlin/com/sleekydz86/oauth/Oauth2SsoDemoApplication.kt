5286760b820e188cf730dc1e449eab960ed7efc4
이경용 <sleekydz86@icloud.com>
2025-11-26 20:26:22 +0900

    fix: 프로젝트 재수정

--- oauth2-sso-demo/src/main/kotlin/com/sleekydz86/oauth/Oauth2SsoDemoApplication.kt
+++ oauth2-sso-demo/src/main/kotlin/com/sleekydz86/oauth/Oauth2SsoDemoApplication.kt
@@ -1,4 +1,4 @@
-package com.sleekydz86
+package com.sleekydz86.oauth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class Oauth2SsoDemoApplication
 
 fun main(args: Array<String>) {
-    runApplication<Oauth2SsoDemoApplication>(*args)
+	runApplication<Oauth2SsoDemoApplication>(*args)
 }
