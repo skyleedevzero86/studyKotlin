9ee53ea8b91293513867deab606fbd22623fb3df
이경용 <sleekydz86@icloud.com>
2025-11-26 20:13:29 +0900

    feat: sso 학습을 위한 Init

---
+++ oauth2-sso-demo/src/main/kotlin/com/sleekydz86/Oauth2SsoDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.sleekydz86
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Oauth2SsoDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<Oauth2SsoDemoApplication>(*args)
+}
---
+++ oauth2-sso-demo/src/test/kotlin/com/sleekydz86/sso/Oauth2SsoDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sleekydz86.sso
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Oauth2SsoDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
