1c67c977219b93a92e4c1b0f789b40c0058dc423
이경용 <sleekydz86@icloud.com>
2026-02-07 03:56:47 +0900

    feat:시맨틱 검색. 벡터 검색 기반 프로젝트 설정 셋팅

---
+++ skkk/src/main/kotlin/com/sleekydz86/skkk/SkkkApplication.kt
@@ -0,0 +1,11 @@
+package com.sleekydz86.skkk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SkkkApplication
+
+fun main(args: Array<String>) {
+    runApplication<SkkkApplication>(*args)
+}
---
+++ skkk/src/test/kotlin/com/sleekydz86/skkk/SkkkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sleekydz86.skkk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SkkkApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
