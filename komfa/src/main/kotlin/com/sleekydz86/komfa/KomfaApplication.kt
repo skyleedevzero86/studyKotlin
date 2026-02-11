bac3f75c9a00aea7af3a14eec012433e729b7eef
이경용 <sleekydz86@icloud.com>
2026-02-11 16:47:25 +0900

    feat: mfa 학습을 위한 셋팅

---
+++ komfa/src/main/kotlin/com/sleekydz86/komfa/KomfaApplication.kt
@@ -0,0 +1,11 @@
+package com.sleekydz86.komfa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KomfaApplication
+
+fun main(args: Array<String>) {
+    runApplication<KomfaApplication>(*args)
+}
