30355641e5c081150b8e22a61a89af7abe138983
이경용 <sleekydz86@naver.com>
2026-06-05 17:22:02 +0900

    feat: QueryDsl 코틀린버전 학습을 위한 프로젝트 Init

---
+++ k_querydsl_study/src/main/kotlin/com/sleekydz86/kqueryds/KQuerydslStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.sleekydz86.kqueryds
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KQuerydslStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<KQuerydslStudyApplication>(*args)
+}
---
+++ k_querydsl_study/src/test/kotlin/com/sleekydz86/kqueryds/KQuerydslStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sleekydz86.kqueryds
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KQuerydslStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
